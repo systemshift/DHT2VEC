e6b0cfda6e5ae77560502aae56ccec2c02f286a8
systemshift <42102034+systemshift@users.noreply.github.com>
2020-10-15 18:47:56 -0700

    Readme (#2)
    * add comments and docstrings
    
    * add readme
    
    * add readme todo
    
    * add use details
    
    * add images
    
    * add example images
    
    * edit readme
    
    * fix image link
    
    * move reqs to top
    
    * remove gitignore
    
    * update gif
    
    * remove image links

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # DHT2VEC
+
+A feature addressable distributed hash table using vectors to search for similarity across a peer-to-peer network. Returning the most similar file.
+For a more detail read, check out the [blog post](https://systemshift.github.io/FAN.html)
+
+
+# Requirements
+
+```
+Tensorflow
+Numpy
+Pillow
+```
+
+# How to use
+
+Project currently in prototype stage, and what exists is the most basic implementation in a controlled environment.
+
+```
+python lookup.py [filepath]
+```
+
+The datalookup has `DATA`, which then has many directories as `DATA/something/`
+
+
+# Example
+
+This is an example that works, other files can still be off target.
+
+```
+python lookup.py 3311335910_36bf189ef6.jpg
+```
+
+![gif](ezgif.com-video-to-gif.gif)
+
+
+
+# TODO
+
+There is no network interface right now, and DHT implementations that exist have hashing baked in. So a DHT needs to be implemented from scratch with the new primitives hardcoded in the codebase.
--- lookup.py
+++ lookup.py
@@ -7,13 +7,16 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
 
-
+# this is the fastest way to get vectors from inputs, but should be an independent module to call as a library if used in production
 model = tf.keras.applications.ResNet50(include_top=False, weights='imagenet', input_shape=(224, 224, 3), pooling='avg')
 
+
+# add user input and dataset file paths into variables to call later
 image_collection = glob.glob('DATA/*/*.jpg')
 input_file = sys.argv[1]
 
 def generate_values(files):
+    '''take file collection paths, and return dict as file: vector'''
     table = {}
     for file in files:
         image = load_img(file, target_size=(224, 224))
@@ -24,6 +27,7 @@ def generate_values(files):
     return table
 
 def anchor_value(file):
+    '''take singe file from user input and return file:vector'''
     image = load_img(file, target_size=(224, 224))
     numpy_image = img_to_array(image)
     input_image = np.expand_dims(numpy_image, axis=0)
@@ -35,6 +39,7 @@ anchor_value = anchor_value(input_file)
 table = generate_values(image_collection)
 
 def triplet_loss(anchor, input1, input2):
+    '''input 3 files: user_input, random1, random2 from file collection and return file name for the lower loss'''
     first_input = ((anchor_value[anchor] - table[input1]) ** 2).mean(axis=1)
     second_input = ((anchor_value[anchor] - table[input2]) ** 2).mean(axis=1)
 
@@ -44,6 +49,7 @@ def triplet_loss(anchor, input1, input2):
         return input1
 
 def binary_search(image_list, anchor):
+    '''take a list of file paths, and return half of the list that is closer to the anchor file'''
     if len(image_list) % 2 != 0:
         image_list.pop()
     
@@ -57,15 +63,24 @@ def binary_search(image_list, anchor):
     return positive_list
     
 def hierarchical_search(image_list, anchor):
+    '''input list of file paths, runs binary search recursively until only one file is left'''
     while len(image_list) != 1:
         image_list = binary_search(image_list, anchor)
     return image_list
 
 result = hierarchical_search(image_collection, input_file)
 
+'''
+TODO: return result to the network layer once lookup is done the the closest file has been found
+'''
 
-# show the results
+
+
+# show the results for demo
 im1 = Image.open(input_file)
 im2 = Image.open(result[0])
 
-Image.fromarray(np.hstack((np.array(im1),np.array(im2)))).show()
+im1 = im1.resize((224, 224))
+im2 = im2.resize((224, 224))
+
+Image.fromarray(np.hstack((np.array(im2),np.array(im1)))).show()
